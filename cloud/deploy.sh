38a5e8272dd3e82601ffa5e110cc0be5abe31b9c
霈政 邱 <paizanmay@gmail.com>
2016-10-12 02:29:19 +0800

    ADD config

--- cloud/deploy.sh
+++ cloud/deploy.sh
@@ -1,2 +1,3 @@
+ecs-cli configure --region ap-northeast-1 --access-key AKIAJLROQZ4DSXSQKKVA --secret-key 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 --cluster hackntu
 ecs-cli compose -f docker-compose-deploy.yml service stop && \
 ecs-cli compose -f docker-compose-deploy.yml service up
